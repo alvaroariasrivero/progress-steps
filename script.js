e28d3fa43da5b0fd493851bf81dead4b41177c44
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-08-29 18:05:26 +0200

    scripts done

--- script.js
+++ script.js
@@ -22,15 +22,24 @@ prev.addEventListener('click', () => {
 });
 
 const update = () => {
-    circles.forEach((circle, index) => {
-        if(index < currentActive){
-            circle.classList.add('active');
-        }else{
-            circle.classList.remove('active');
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
         }
-    });
-};
+    })
 
-const actives = document.querySelectorAll('active');
+    const actives = document.querySelectorAll('.active')
 
-// progress.style.width = (actives.length - 1)
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+}
